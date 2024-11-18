b8ddb8847e46adc0be6849d5ce007c5dffa4390d
kili <kili>
2024-11-18 09:28:17 +0100

    Initial Commit

---
+++ FoodPin_3/FoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinApp.swift
+//  FoodPin
+//
+//  Created by Thomas Helml on 23.11.21.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
